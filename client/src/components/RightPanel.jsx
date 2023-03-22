1e5588c8558184bf7f8b358f3ab57ed9b73e7391
achal6076 <Achal6076@gmail.com>
2023-03-22 18:49:49 +0530

    first commit step_up_calculator frontend

---
+++ client/src/components/RightPanel.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Calculator from "./Calculator";
+function RightPanel() {
+  return (
+    <div className="rightPanel">
+        
+        <h3>←Back</h3>
+        <div className="calculator">
+          <Calculator />
+        </div>
+      </div>
+
+  );
+}
+
+export default RightPanel;
---
+++ client/src/components/SipCalculator.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SideBar from './sideBar'
+import RightPanel from './RightPanel'
+
+ function SipCalculator() {
+  return (
+    <div className='sipCalculator'>
+        <SideBar/>
+        <RightPanel /> 
+    </div>
+    
+    
+  )
+}
+
+export default SipCalculator;
+
